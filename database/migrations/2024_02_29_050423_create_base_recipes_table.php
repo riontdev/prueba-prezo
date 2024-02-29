6c7deae77a855dd3ebaf1ea4a97434cb6b9540aa
Jorge Mujica <riontdev@gmail.com>
2024-02-29 19:31:23 +0000

    added new column in migration base_recipes

--- database/migrations/2024_02_29_050423_create_base_recipes_table.php
+++ database/migrations/2024_02_29_050423_create_base_recipes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('base_recipes', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
+            $table->decimal('total', 8, 2);
             $table->timestamps();
         });
     }
